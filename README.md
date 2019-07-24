847d785f4ca28cb5eb21ce531f874e2ef7d92356
Konstantin Beloborodov <kosta.white@gmail.com>
2019-07-25 00:26:54 +0300

    Release version 1.0

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The module calculating most frequent words in present text file.
+
+
+# Project description
+
+Method get_most_frequent_words(text) in the module return tuple of most frequent words in descending order.
+
+For console using pass text file as argument.
+For example like this:
+lang_frequency.py <text_file.txt>
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,41 @@
+import re
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as file_handler:
+        return re.findall(r"[\w']+", file_handler.read().lower())
 
 
 def get_most_frequent_words(text):
-    pass
+    words_dict = {}
+    for word in text:
+        if word in words_dict.keys():
+            words_dict[word] += 1
+        else:
+            words_dict[word] = 1
+    return sorted(
+        words_dict.items(),
+        key=lambda value: value[1],
+        reverse=True
+    )
 
 
 if __name__ == '__main__':
-    pass
+    sys_arg = ''
+    if len(sys.argv) > 1:
+        sys_arg = sys.argv[1]
+        try:
+            for line in get_most_frequent_words(load_data(sys_arg)):
+                print(line)
+        except FileNotFoundError:
+            print('File "', sys_arg, '" not found.', sep='')
+    else:
+        print(
+            '''
+            The module calculating most frequent words in present text file.
+            For using this module pass text file as argument.
+            For example like this:
+            lang_frequency.py <text_file.txt>
+            '''
+        )
